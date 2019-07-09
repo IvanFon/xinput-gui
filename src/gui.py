da55d0cca85e0b2280532c675253b05e489017fc
Ivan Fonseca <ivanfon@riseup.net>
2019-07-09 01:44:35 -0400

    Add settings dialog, allow hiding property IDs

--- src/gui.py
+++ src/gui.py
@@ -27,11 +27,12 @@ class Gui:
 
         # Main window widgets
         self.win_app = self.builder.get_object("win_app")
+        self.btn_edit = self.builder.get_object("btn_edit")
         self.store_devices = self.builder.get_object("store_devices")
         self.store_props = self.builder.get_object("store_props")
         self.tree_devices_selection = self.builder.get_object("tree_devices_selection")
         self.tree_props_selection = self.builder.get_object("tree_props_selection")
-        self.btn_edit = self.builder.get_object("btn_edit")
+        self.tree_column_props_id = self.builder.get_object("tree_column_props_id")
         
         # Edit window widgets
         self.win_edit = self.builder.get_object("win_edit")
@@ -39,9 +40,14 @@ class Gui:
         self.entry_new_val = self.builder.get_object("entry_new_val")
         self.btn_edit_cancel = self.builder.get_object("btn_edit_cancel")
         self.btn_edit_apply = self.builder.get_object("btn_edit_apply")
+
+        # Settings window widgets
+        self.win_settings = self.builder.get_object("win_settings")
+        self.btn_settings_save = self.builder.get_object("btn_settings_save")
+        self.chk_hide_prop_ids = self.builder.get_object("chk_hide_prop_ids")
         
         self.refresh_devices()
-        self.win_app.set_title('Xinput GUI {}'.format(version))
+        self.win_app.set_title("Xinput GUI {}".format(version))
         self.win_app.show_all()
 
         Gtk.main()
@@ -88,13 +94,41 @@ class Gui:
             'val': model[treeiter][2],
             })
 
+    def show_settings_window(self):
+        # Get settings and update controls
+        if self.tree_column_props_id.get_visible():
+            self.chk_hide_prop_ids.set_active(False)
+        else:
+            self.chk_hide_prop_ids.set_active(True)
+
+        self.btn_settings_save.set_sensitive(False)
+        self.win_settings.show_all()
+
+    def save_settings(self):
+        # Get settings
+        hide_prop_ids = self.chk_hide_prop_ids.get_active()
+
+        # Save settings
+        # TODO
+
+        # Apply settings to active program
+        if hide_prop_ids:
+            self.tree_column_props_id.set_visible(False)
+        else:
+            self.tree_column_props_id.set_visible(True)
+    
     class SignalHandler:
         def __init__(self, gui):
             self.gui = gui
 
+        # Main window signals
+
         def on_win_app_destroy(self, *args):
             Gtk.main_quit()
 
+        def on_menu_settings_activate(self, menu: Gtk.MenuItem):
+            self.gui.show_settings_window()
+        
         def on_btn_close_clicked(self, button: Gtk.Button):
             Gtk.main_quit()
 
@@ -114,6 +148,8 @@ class Gui:
             self.gui.entry_new_val.set_text(prop['val'])
             self.gui.win_edit.show_all()
             self.gui.entry_new_val.grab_focus()
+
+        # Edit window signals
 
         def on_entry_new_val_activate(self, entry: Gtk.Entry):
             self.gui.btn_edit_apply.clicked()
@@ -136,3 +172,15 @@ class Gui:
         
         def on_btn_edit_cancel_clicked(self, button: Gtk.Button):
             self.gui.win_edit.hide()
+
+        # Settings window signals
+
+        def on_btn_settings_save_clicked(self, button: Gtk.Button):
+            self.gui.save_settings()
+            self.gui.win_settings.hide()
+
+        def on_btn_settings_cancel_clicked(self, button: Gtk.Button):
+            self.gui.win_settings.hide()
+
+        def on_chk_hide_prop_ids_toggled(self, chk_btn: Gtk.CheckButton):
+            self.gui.btn_settings_save.set_sensitive(True)
